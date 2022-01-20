4127cc19c9875c7eac2be17c0f9334db6effcafe
Paulo Moraes <pmoraes1080@gmail.com>
2022-01-20 14:55:30 -0300

    ...

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://admin-portal-over.herokuapp.com"
+    //baseURL: "https://admin-portal-over.herokuapp.com"
+    baseURL: "http://localhost:8080"
 })
 
 api.interceptors.request.use(async config => {
    let response = localStorage.getItem("over_token");
    
    if(response) {
-        var token = `Bearer ${response}`
+        var token = `Bearer ${response}`;
         config.headers.Authorization = token;
    }
 
